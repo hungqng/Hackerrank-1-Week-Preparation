8cb2433aea93620ff3b361445e88c9f1ee89ced4
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-02-27 22:22:34 -0800

    Add files via upload
    Day 3

---
+++ Tower Breakers.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    # Write your code here
+    if m == 1:
+        return 2
+    else:
+        if n % 2 == 1:
+            return 1
+        else:
+            return 2
