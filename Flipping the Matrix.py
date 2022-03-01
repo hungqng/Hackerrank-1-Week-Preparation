ed7a316f59c7a0527571f9ad50f66820ddc321db
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-02-28 23:00:29 -0800

    Add files via upload
    Solution Day 2

--- Flipping the Matrix.py
+++ Flipping the Matrix.py
@@ -16,3 +16,51 @@
 #                 maxv = max(l)
 #                 v+= maxv
 # return v
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'flippingMatrix' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+#
+
+def flippingMatrix(matrix):
+    # Write your code here
+    n = int(len(matrix) /  2)
+    maxValue = 0
+    total = 0
+    
+    for r in range(n):
+        for c in range(n):
+            maxValue += max(max(matrix[r][c],matrix[r][2*n-c-1]),
+                            max(matrix[2*n-r-1][c],matrix[2*n-r-1][2*n-c-1]))
+    return maxValue
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        matrix = []
+
+        for _ in range(2 * n):
+            matrix.append(list(map(int, input().rstrip().split())))
+
+        result = flippingMatrix(matrix)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
